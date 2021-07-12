031baab4d09ba1cbe87ade61bd1c8d73b0026460
eilmadc <eilmadc@gmail.com>
2021-07-12 10:55:04 +0200

    login methods + registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: './home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
+    path: 'login', 
+    loadChildren: () => import('./components/login/login.component').then( m => m.LoginComponent)
+  },
+  {
+    path: 'folder', 
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./components/login/registration/registration.module').then( m => m.RegistrationPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -6,8 +6,39 @@ import { FolderPage } from './folder.page';
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
+    component: FolderPage,
+    children:[
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'mapas',
+        loadChildren: () => import('./mapas/mapas.module').then( m => m.MapasPageModule)
+      },
+      {
+        path: 'listado',
+        loadChildren: () => import('./listado/listado.module').then( m => m.ListadoPageModule)
+      },
+      {
+        path: 'camara',
+        loadChildren: () => import('./camara/camara.module').then( m => m.CamaraPageModule)
+      },
+  {
+    path: 'consultas',
+    loadChildren: () => import('./consultas/consultas.module').then( m => m.ConsultasPageModule)
+  },
+  {
+    path: 'aboutus',
+    loadChildren: () => import('./aboutus/aboutus.module').then( m => m.AboutusPageModule)
+  },
+  {
+    path: 'contacto',
+    loadChildren: () => import('./contacto/contacto.module').then( m => m.ContactoPageModule)
   }
+    ]
+  },
+ 
 ];
 
 @NgModule({
