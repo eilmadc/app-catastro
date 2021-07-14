94ce73f0f564ba5baee1b318b8db9c6087197a36
JPQueirozPerez <jpqueirozperez@gmail.com>
2021-07-14 17:23:25 +0200

    marcadores

--- src/app/folder/mapas/mapas.page.ts
+++ src/app/folder/mapas/mapas.page.ts
@@ -1,4 +1,4 @@
-import { AttributeMarker } from '@angular/compiler/src/core';
+// import { AttributeMarker } from '@angular/compiler/src/core';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 // import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
@@ -127,6 +127,16 @@ export class MapasPage implements OnInit {
 
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
       this.renderMarkers();
+      // let marker: Marker = this.map.addMarkerSync({
+      //   title: 'Prueba',
+      //   icon: 'blue',
+      //   animation: 'DROP',
+      //   position: {
+      //     lat: 41.3918415,
+      //     lng: 2.1166554,
+      //   }
+      // });
+      // marker.showInfoWindow();
       mapEle.classList.add('show-map');
     });
 
@@ -138,6 +148,7 @@ export class MapasPage implements OnInit {
     });
 
     this.addInfoWindowToMarker(this.markers);
+
 
   })
  .catch((error: any) => {
@@ -153,10 +164,23 @@ export class MapasPage implements OnInit {
 
   addMarker(marker: Marker) {
     //this.addInfoWindowToMarker(marker);
+
+    const svgMarker = {
+      path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+      fillColor: "darkorange",
+      fillOpacity: 0.6,
+      strokeWeight: 0,
+      rotation: 0,
+      scale: 2,
+      anchor: new google.maps.Point(15, 30),
+    };  
+
     let markers = new google.maps.Marker({
       position: marker.position,
       map: this.map,
       title: marker.title,
+      icon: svgMarker,
+      // animation: 'DROP',
     });
     return markers;
   }
@@ -188,7 +212,7 @@ export class MapasPage implements OnInit {
     
     console.log('content');
                         
-    let infoWindow = new google.maps.infoWindow({
+    let infoWindow = new google.maps.infoWindow({   
       content: infoWindowContent
     });
     console.log('infowindow');
