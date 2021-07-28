70a7d46d102f8244db1de0603ff7222c9e9c971c
Juan Pablo Queiroz Pérez <51370813+JPQueirozPerez@users.noreply.github.com>
2021-07-28 22:07:49 +0200

    Update mapas.page.ts

--- src/app/folder/mapas/mapas.page.ts
+++ src/app/folder/mapas/mapas.page.ts
@@ -69,9 +69,19 @@ export class MapasPage implements OnInit {
         //marcadores genéricos
         const svgMarker = {
           path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-          //los marcadores tienen colores distinos dependiendo de si están o no en el listado de favoritos
-          fillColor: (data.favorito == true && showFav == true) ? "purple": "darkorange",
+          fillColor: "darkorange",
           fillOpacity: 0.8,
+          strokeWeight: 0,
+          rotation: 0,
+          scale: 2,
+          anchor: new google.maps.Point(15, 30),
+        };
+
+        //marcadores en favoritos
+        const favMarker = {
+          path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+          fillColor: "purple",
+          fillOpacity: 0.9,
           strokeWeight: 0,
           rotation: 0,
           scale: 2,
@@ -86,7 +96,8 @@ export class MapasPage implements OnInit {
                     position: myLatlng,
                     map: this.map,
                     title: data.nota,   
-                    icon: svgMarker
+                    //los marcadores tienen colores distinos dependiendo de si están o no en el listado de favoritos
+                    icon: (data.favorito == true && showFav == true) ? favMarker : svgMarker
                   });
             
               //Evento de click en cada marcador
