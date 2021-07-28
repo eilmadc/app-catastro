fa84f6f62a83397582c35be777b27e8a2c202c67
lisardogayan <lisardogayan@gmail.com>
2021-07-28 19:36:26 +0200

    Notification 2

--- src/app/shared/services/fcm.service.ts
+++ src/app/shared/services/fcm.service.ts
@@ -7,18 +7,23 @@ import {
   Token,
 } from '@capacitor/push-notifications';
 import { Router } from '@angular/router';
- 
-// const { PushNotifications } = Plugins;
+import { AlertController, ToastController } from '@ionic/angular';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FcmService {
 
-  constructor(private router: Router) { }
+  
+
+  constructor(private router: Router,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController) { }
 
   initPush() {
-    if (Capacitor.platform !== 'web') {
+    if (Capacitor.getPlatform() !== 'web') {
       this.registerPush();
     }
   }
@@ -38,27 +43,52 @@ export class FcmService {
     });
 
     PushNotifications.addListener('registration', (token: Token) => {
-      alert('Push registration success, token: ' + token.value);
+      //alert('Push registration success, token: ' + token.value);
+      console.log('Push registration success, token: ' + token.value);
+      // const header = 'Registrado en la aplicación';
+      // this.presentAlert (header, token);
+      this.presentToast('Token registered:' + token.value);
     });
 
     PushNotifications.addListener('registrationError', (error: any) => {
-      alert('Error on registration: ' + JSON.stringify(error));
+      //alert('Error on registration: ' + JSON.stringify(error));
+      this.presentAlert(error.header, error.message);
     });
 
-    PushNotifications.addListener(
-      'pushNotificationReceived',
+    PushNotifications.addListener('pushNotificationReceived',
       (notification: PushNotificationSchema) => {
-        alert('Push received: ' + JSON.stringify(notification));
+         //alert('Push received: ' + JSON.stringify(notification));
+         this.presentAlert(notification.title, notification.body);
       },
     );
 
     PushNotifications.addListener(
       'pushNotificationActionPerformed',
       (notification: ActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
+         //alert('Push action performed: ' + JSON.stringify(notification));
+         const header = JSON.stringify(notification.actionId);
+         const body = "Notificación leida";
+         this.presentAlert(header, body);
       },
     );
+  }
 
-    
+
+  async presentAlert (header, message) {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK']
+    });      
+  }
+
+  async presentToast(mes: string) {
+    const toast = await this.toastCtrl.create({
+      message: mes,
+      position: 'middle',
+      color: '#0000F0',
+      duration: 2000
+    });
+    toast.present();
   }
 }
